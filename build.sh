fe7270fd0588506c959f9f3fee82f341c681e1d6
Nikita Tsukanov <keks9n@gmail.com>
2025-10-16 18:03:59 +0500

    try

--- build.sh
+++ build.sh
@@ -7,9 +7,13 @@ cp engine/scripts/standard.gclient .gclient
 gclient sync -D
 
 GN_ARGS=""
+BUILDDIR=out/host_release
 if [[ "$(uname)" == "Darwin" ]]; then
   GN_ARGS="--mac-cpu=arm64"
+  BUILDDIR=out/host_release_arm64
 fi
 ./engine/src/flutter/tools/gn $GN_ARGS --runtime-mode release
 
-ninja -C engine/src/out/host_release sdk
+ninja -C $BUILDDIR sdk
+
+find .|grep 'impeller_sdk.zip$'
