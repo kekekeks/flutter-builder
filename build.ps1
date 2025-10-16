2cf7a80f7f43cb56ce082a4ec802365cf90ea0c5
Nikita Tsukanov <keks9n@gmail.com>
2025-10-16 20:51:01 +0500

    vs path

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 $env:DEPOT_TOOLS_WIN_TOOLCHAIN = "0"
-$env:GYP_MSVS_OVERRIDE_PATH = "C:\Program Files\Microsoft Visual Studio\2022\Community"
+$env:GYP_MSVS_OVERRIDE_PATH = "C:\Program Files\Microsoft Visual Studio\2022\\Enterprise"
 $env:PATH = "$(Get-Location)\depot_tools;$env:PATH"
 Set-Location flutter
 Copy-Item engine\scripts\standard.gclient .gclient -Force
