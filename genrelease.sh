1a0403396486d3ccd53f49e6cf739c8ebe995f87
Nikita Tsukanov <keks9n@gmail.com>
2025-10-20 10:32:34 +0400

    Hurr-durr 40hex names not allowed

--- genrelease.sh
+++ genrelease.sh
@@ -4,7 +4,7 @@ set -e
 git submodule status flutter
 FLUTTER_ID=`git submodule status flutter|awk '{print $1}'|sed 's/-//'`
 echo "Build for commit https://github.com/flutter/flutter/commit/$FLUTTER_ID" > release.txt  
-echo "RELEASE_ID=$FLUTTER_ID" >> $GITHUB_ENV
+echo "RELEASE_ID=flutter_$FLUTTER_ID" >> $GITHUB_ENV
 
 mkdir files
 for dir in artifacts/*; do
